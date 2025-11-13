9d845732e573dfe43e952b363f86deb3a11f4c6f
Kadambu Ramachandra <ajaykr@ad.cs.umu.se>
2025-11-13 17:23:57 +0100

    Log classification results

--- ltn_model/main.py
+++ ltn_model/main.py
@@ -4,7 +4,7 @@ import torch
 from torch.utils.data import DataLoader
 import wandb
 
-def getreconstruction(dataloader_test, Front, Right, Up, dec, rot_plus, rot_minus):
+def getreconstruction(dataloader_test, Front, Right, Up, dec, rot_plus, rot_minus, digits):
     print("showreconstruction")
     initial_state_img_t, action_t, next_state_img_t = next(iter(dataloader_test))
     reconstructed = None
@@ -13,6 +13,10 @@ def getreconstruction(dataloader_test, Front, Right, Up, dec, rot_plus, rot_minu
         front = Front(initial_state_img_t.to("cuda")) #.view(initial_state_img.shape[0], -1)
         right = Right(initial_state_img_t.to("cuda")) #.view(initial_state_img.shape[0], -1)
         up = Up(initial_state_img_t.to("cuda")) #.view(initial_state_img.shape[0], -1)
+
+        front_digit_classification = torch.argmax(torch.tensor([P(front[0].unsqueeze(0)).item() for P in digits])).item()
+        right_digit_classification = torch.argmax(torch.tensor([P(right[0].unsqueeze(0)).item() for P in digits])).item()
+        up_digit_classification = torch.argmax(torch.tensor([P(up[0].unsqueeze(0)).item() for P in digits])).item()
 
         if action_t[0].item() == 0:
             decoded = dec(front[0].unsqueeze(0), rot_plus(right[0].unsqueeze(0)), front[0].unsqueeze(0)) 
@@ -33,14 +37,20 @@ def getreconstruction(dataloader_test, Front, Right, Up, dec, rot_plus, rot_minu
             decoded = dec(rot_minus(front[0].unsqueeze(0)), right[0].unsqueeze(0), rot_minus(right[0].unsqueeze(0))) 
             reconstructed = torch.nn.functional.sigmoid(decoded[0])
     
-    return { "GT_image_1": initial_state_img_t[0], "GT_image_2": next_state_img_t[0], "Reconstruction": reconstructed}
+    return { "GT_image_1": initial_state_img_t[0], "GT_image_2": next_state_img_t[0], "Reconstruction": reconstructed, "front_digit": front_digit_classification, "right_digit": right_digit_classification, "up_digit": up_digit_classification}
 
 
-def train(ltn_obj, optimizer, dataloader_train, dataloader_test, epochs):
+def log(ltn_obj, dataloader_test, epoch, train_loss, train_sat):
+    log_dict = getreconstruction(dataloader_test, ltn_obj.logic_models.front, ltn_obj.logic_models.right, ltn_obj.logic_models.up, ltn_obj.logic_models.dec, ltn_obj.logic_models.rot_plus, ltn_obj.logic_models.rot_minus, ltn_obj.logic_models.digits)
+    wandb.log({ "gt_image_1": wandb.Image(log_dict["GT_image_1"]), "gt_image_2": wandb.Image(log_dict["GT_image_2"]), "reconstruction": wandb.Image(log_dict["Reconstruction"]), 
+                "front_digit": log_dict["front_digit"], "right_digit": log_dict["right_digit"], "up_digit": log_dict["up_digit"], "Epoch": epoch, "Loss": train_loss, "Sat": train_sat })
+
+def train(ltn_obj, optimizer, dataloader_train, dataloader_test, epochs, steps_log=20):
     for epoch in range(epochs):
         train_loss = 0
         train_sat = 0
 
+        i = 0
         for init_image_, action_, next_image_ in dataloader_train:
             sat_val = ltn_obj.compute_sat(init_image_.float().to("cuda"), action_.float().to("cuda"), next_image_.float().to("cuda"))
             
@@ -52,10 +62,14 @@ def train(ltn_obj, optimizer, dataloader_train, dataloader_test, epochs):
             train_loss += loss.item()
             train_sat += sat_val
 
+            if i % steps_log == 0:
+                log(ltn_obj=ltn_obj, dataloader_test=dataloader_test, epoch=epoch, train_loss=train_loss, train_sat=train_sat)
+
+            i += 1
+
         train_loss = train_loss/len(dataloader_train)
         train_sat = train_sat/len(dataloader_train)
-        log_dict = getreconstruction(dataloader_test, ltn_obj.logic_models.front, ltn_obj.logic_models.right, ltn_obj.logic_models.up, ltn_obj.logic_models.dec, ltn_obj.logic_models.rot_plus, ltn_obj.logic_models.rot_minus)
-        wandb.log({ "gt_image_1": wandb.Image(log_dict["GT_image_1"]), "gt_image_2": wandb.Image(log_dict["GT_image_2"]), "reconstruction": wandb.Image(log_dict["Reconstruction"]), "Epoch": epoch, "Loss": train_loss, "Sat": train_sat })
+        log(ltn_obj=ltn_obj, dataloader_test=dataloader_test, epoch=epoch, train_loss=train_loss, train_sat=train_sat)
         print(f"Epoch: {epoch} | Train loss: {train_loss} | Train Sat:{train_sat}")
 
 def main(dataset_train_path, dataset_test_path, login_key, batch_size=32, lr=0.00001, epochs=1):
