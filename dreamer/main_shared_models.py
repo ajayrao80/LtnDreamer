e1911b4cc630f69abbcb9f7e9739e0f2cbadc08c
Ajay Rao <krrao.rao@gmail.com>
2025-12-02 14:52:49 +0100

    Update main_shared_models.py

--- dreamer/main_shared_models.py
+++ dreamer/main_shared_models.py
@@ -253,7 +253,8 @@ def main(lr, epochs, embed_dim, stoch_dim, deter_dim, dataset_train_path, datase
         logic_weight = logic_weight*logic_decay_rate
     
     wandb.finish()
-    save_model(encoder, epochs, "encoder", model_save_path)
+    #save_model(encoder, epochs, "encoder", model_save_path)
+    logic_loss_object.ltn_models.save_all_models(model_save_path)
     save_model(decoder, epochs, "decoder", model_save_path)
     save_model(rssm, epochs, "rssm", model_save_path)
     
